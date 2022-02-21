0f794f0c97472cc066f56069e9074e2c567014cc
fontesecaBarreto <lucasbfonte@gmail.com>
2022-02-21 19:09:01 -0300

    react integration

---
+++ examples/react-examples/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ComponentTeste from "../../lib/react";
+ReactDOM.render(
+    <React.StrictMode>
+        <div className='fck-examples'> 
+            <h1> fck-location-selector</h1>
+            <h3> Autor: 
+                <a href={"https://github.com/fonsecaBarreto"}> Lucas Fonseca </a>
+            </h3>
+            <ComponentTeste/>
+        </div>
+</React.StrictMode>, document.getElementById("root") );
+
+
+
+
+
