24fb835f415ca52d690ec1ecd4854d4a5eae9c38
fontesecaBarreto <lucasbfonte@gmail.com>
2022-02-21 19:36:17 -0300

    1935

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-
 ## fck-brasil-location-selector ☈
 Seleção de localidade no territorio brasileiro através da integração com a API publica do IBGE
--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { get_regioes } from '../core/ibge-services'
 
 export const SelectorComponenet = () =>{
+    const [ regioes, setRegioes ] = useState([])
+    useEffect(()=>{
+        get_regioes().then(setRegioes)
+    }) 
     return (
         <div>
-            Hello World
+
+            <h3> A baixo as Regioes do Brasil</h3>
+            {JSON.stringify(regioes)}
         </div>
     )
 }
